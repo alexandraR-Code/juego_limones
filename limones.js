becc6778a85f02be8f9b0d00f82c70e582ed0ce1
Alexandra Ramirez <alexa090114@gmail.com>
2026-04-12 20:08:15 -0500

    agregamos las funciones dibujarSuelo y dibujarPersonaje

--- limones.js
+++ limones.js
@@ -5,18 +5,23 @@ const ALTURA_SUELO = 40;
 const ALTURA_PERSONAJE = 60;
 const ANCHO_PERSONAJE = 40;
 
+let personajeX = canvas.width/2;
+
+
 function iniciarJuego(){
     dibujarSuelo();
     dibujarPersonaje();
+
 }
-
-
 function dibujarSuelo(){
     ctx.fillStyle ="green";
     ctx.fillRect(0,canvas.height-ALTURA_SUELO,canvas.width, ALTURA_SUELO);
 }
-
 function dibujarPersonaje(){
     ctx.fillStyle = "yellow";
-    ctx.fillRect(canvas.width/2, canvas.height-(ALTURA_SUELO+ALTURA_PERSONAJE), ANCHO_PERSONAJE, ALTURA_PERSONAJE);
+    ctx.fillRect(personajeX, canvas.height-(ALTURA_SUELO+ALTURA_PERSONAJE), ANCHO_PERSONAJE, ALTURA_PERSONAJE);
+}
+function moverIzquierda(){
+    personajeX -= 10;
+    dibujarPersonaje();
 }
