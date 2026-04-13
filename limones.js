1f065ced4edb58ee7115e5d111fbb5752eb5511c
Alexandra Ramirez <alexa090114@gmail.com>
2026-04-13 18:04:25 -0500

    feat: finalizar lógica de juego y corrección de errores críticos

--- limones.js
+++ limones.js
@@ -14,12 +14,25 @@ let limonY = 5;
 let puntaje = 0;
 let vidas = 3;
 let velocidadCaida = 200;
+let tiempo = 30;
+let detenerJ;
+let intervaloTiempo; 
+let intervaloLimon;
 
 function iniciarJuego(){
-    setInterval(bajarLimon, velocidadCaida); // primer parametro una funcion y como segundo parametro el tiempo en milisegundos 
-    dibujarSuelo();
-    dibujarPersonaje();
+    clearInterval(intervaloLimon);
+    clearInterval(intervaloTiempo);
+
+    mostrarEnSpan("txtTiempo", tiempo);
+    mostrarEnSpan("txtVidas", vidas);
+    mostrarEnSpan("txtPuntaje", puntaje);
+
+    intervaloLimon= setInterval(bajarLimon, velocidadCaida);
+    intervaloTiempo = setInterval(restarTiempo, 1000);
+
+
     aparecerLimon();
+
 
 
 }
@@ -41,10 +54,11 @@ function moverDerecha(){
 }
 
 function actualizarPantalla(){
-    dibujarCanva();
+    ctx.clearRect(0,0,canvas.width,canvas.height);
     dibujarSuelo();
     dibujarPersonaje();
     dibujarLimon();
+
     
 }
 function dibujarCanva(){
@@ -72,6 +86,20 @@ function detectarAtrapado(){
     }
 }
 
+function detectarPiso(){
+    if(limonY + ALTURA_LIMON == canvas.height -ALTURA_SUELO){
+       aparecerLimon();
+       vidas = vidas -1; 
+       mostrarEnSpan("txtVidas", vidas);
+       mostrarEnSpan("txtPuntaje", puntaje);
+       if (vidas <=0){
+        finalizarJuego("GAME OVER");
+       }else{
+        aparecerLimon();
+       }
+    }
+}
+
 
 function aparecerLimon(){
     limonX= generarAleatorio(0,canvas.width-ANCHO_LIMON);
@@ -79,12 +107,34 @@ function aparecerLimon(){
     actualizarPantalla();
 }
 
-function detectarPiso(){
-    if(limonY + ALTURA_LIMON == canvas.height -ALTURA_SUELO){
-       aparecerLimon();
-       vidas = vidas -1; 
-       mostrarEnSpan("txtVidas", vidas);
+
+
+function restarTiempo(){
+    tiempo = tiempo - 1;
+    mostrarEnSpan("txtTiempo", tiempo);
+    if(tiempo <= 0){
+       finalizarJuego("Se acabo el tiempo")    
     }
 }
+function finalizarJuego (mensaje){
+    clearInterval(detenerJ);
+    clearInterval(intervaloTiempo);
+    alert("GAME OVER! " + " Puntaje final: " + puntaje);
+    location.reload();
+}
+
+function aleatoria(min, max){
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+function mostrarEnSpan(idSpan, valor) {
+    let componente = document.getElementById(idSpan)
+    if(componente)componente.textContent = valor;
+}
+function reiniciarJuego(){
+    location.reload();
+}
+
+
+
    
 
